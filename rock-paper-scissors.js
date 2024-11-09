98802c01dd77272ab052ab225042311c51e07fa7
MUTHUMANIKANDAN11 <128957905+MUTHUMANIKANDAN11@users.noreply.github.com>
2024-11-09 13:43:35 +0530

    Update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -123,11 +123,11 @@ function generateComputerMove(){
     let computerMove = '';
 
     if(randomNumber>=0 && randomNumber<1/3){
-        computerMove = 'Rock';
+        computerMove = 'rock';
     } else if (randomNumber>=1/3 && randomNumber<2/3){
-        computerMove = 'Paper';
+        computerMove = 'paper';
     } else if (randomNumber>=2/3 && randomNumber<1){
-        computerMove = 'Scissors';
+        computerMove = 'scissors';
     }
 
     return computerMove;
